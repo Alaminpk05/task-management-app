d2d5f67dab16fbdbbdf171ca6e462e15e34b6d02
Alaminpk05 <mdalaminpk05@gmail.com>
2024-12-07 23:16:40 +0600

    done reports

--- lib/features/report/presentation/reports.dart
+++ lib/features/report/presentation/reports.dart
@@ -1,28 +1,31 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sizer/sizer.dart';
+import 'package:task_management_app/features/task/bloc/task_bloc.dart';
 import 'package:task_management_app/features/widgets/text_widget.dart';
 
-
 class Reports extends StatelessWidget {
-   double radius= 30.sp ;
-   double centerRadius=25.sp;
+  final double radius = 30.sp;
+  final double centerRadius = 25.sp;
 
   Reports({super.key});
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-
         body: Container(
           margin: EdgeInsets.symmetric(horizontal: 3.w, vertical: 3.h),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              textWidget(text:'Task Report',
+              textWidget(
+                text: 'Task Report',
                 fontsize: 19.sp,
-                fontwight: FontWeight.bold, color: Colors.black,),
+                fontwight: FontWeight.bold,
+                color: Colors.black,
+              ),
               Stack(
                 children: [
                   // Row containing Pie Chart and Legend
@@ -30,51 +33,67 @@ class Reports extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       // Pie Chart within AspectRatio for equal width and height
-                      SizedBox(
-                        height: 30.h,
-                        width: 45.w,
-                        child: PieChart(
-                          PieChartData(
-                            sections: [
-                              PieChartSectionData(
-                                value: 40,
-                                color: Colors.blue,
-                                title: '40%',
-                                radius: radius,
-                                titleStyle: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
+                      BlocBuilder<TaskBloc, TaskState>(
+                        builder: (context, state) {
+                          final done = state.taskList
+                              .where((task) => task.isComplete == true);
+                          final undone = state.taskList
+                              .where((task) => task.isComplete == false);
+                          final archive = state.taskList
+                              .where((task) => task.isArchive == true);
+                          final donecal =
+                              (done.length / state.taskList.length) * 100;
+                          final undonecal =
+                              (undone.length / state.taskList.length) * 100;
+                          final archivecal =
+                              (archive.length / state.taskList.length) * 100;
 
-                              PieChartSectionData(
-                                value: 15,
-                                color: Colors.purple,
-                                title: '15%',
-                                radius: radius,
-                                titleStyle: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
+                          return SizedBox(
+                            height: 30.h,
+                            width: 45.w,
+                            child: PieChart(
+                              PieChartData(
+                                sections: [
+                                  PieChartSectionData(
+                                    value: archivecal,
+                                    color: Colors.blue,
+                                    title: '${archivecal.toInt()}%',
+                                    radius: radius,
+                                    titleStyle: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  PieChartSectionData(
+                                    value: undonecal,
+                                    color: Colors.purple,
+                                    title: "${undonecal.toInt()}%",
+                                    radius: radius,
+                                    titleStyle: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  PieChartSectionData(
+                                    value: donecal,
+                                    color: Colors.green,
+                                    title:'${donecal.toInt()}%',
+                                    radius: radius,
+                                    titleStyle: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                                centerSpaceRadius: centerRadius,
+                                sectionsSpace: 2,
                               ),
-                              PieChartSectionData(
-                                value: 15,
-                                color: Colors.green,
-                                title: '15%',
-                                radius: radius,
-                                titleStyle: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                            centerSpaceRadius: centerRadius,
-                            sectionsSpace: 2,
-                          ),
-                        ),
+                            ),
+                          );
+                        },
                       ),
                       // Spacing between chart and legend
                       SizedBox(width: 5.w),
@@ -83,11 +102,20 @@ class Reports extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Indicator(color: Colors.blue, text: 'First', isSquare: true),
+                          Indicator(
+                              color: Colors.blue,
+                              text: 'Archive Tasks',
+                              isSquare: true),
                           SizedBox(height: 1.h),
-                          Indicator(color: Colors.purple, text: 'Third', isSquare: true),
+                          Indicator(
+                              color: Colors.purple,
+                              text: 'Undone Tasks',
+                              isSquare: true),
                           SizedBox(height: 1.h),
-                          Indicator(color: Colors.green, text: 'Fourth', isSquare: true),
+                          Indicator(
+                              color: Colors.green,
+                              text: 'Done Tasks',
+                              isSquare: true),
                         ],
                       ),
                     ],
